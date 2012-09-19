4bb995f28836c597a2e5c06bbfe1fd9dddf530d7
aharris88 <aharris88@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-19 17:23:18 +0000

    first update
    git-svn-id: http://plugins.svn.wordpress.org/starbucks-reload/trunk@601096 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Plugin Name ===
+Contributors: aharris88
+Donate link: http://example.com/
+Tags: donate, starbucks, coffee
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin allows you to put a widget on your site that allows visitors to reload your Starbucks card.
+
+== Description ==
+
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Put the widget in a sidebar
+4. Enter your Starbucks card number and save
+5. You can also customize the title and the messge underneath the title
+
+== Frequently Asked Questions ==
+
+
+
+== Screenshots ==
+
+1. widget.png
+2. settings.png
+
+== Changelog ==
+
+= 1.0 =
+* This is the first version
+
+== Upgrade Notice ==
+
+= 1.0 =
+This is the first version
---
+++ starbucks.php
@@ -0,0 +1,100 @@
+<?php
+/*
+Plugin Name: Starbucks Reload
+Plugin URI: http://www.adamwadeharris.com/wordpress/plugins/starbucks
+Description: This plugin allows you to put a widget on your site that allows visitors to reload your Starbucks card.
+Author: Adam Harris
+Version: 1.0
+Author URLI: http://www.adamwadeharris.com
+
+Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : PLUGIN AUTHOR EMAIL)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+class starbucksWidget extends WP_Widget{
+	function starbucksWidget(){
+		$widget_options = array(
+			'classname' => 'starbucks-widget',
+			'description' => 'Let visitors reload your Starbucks card.'
+		);
+		parent::WP_Widget('starbucks-widget', 'Starbucks Widget', $widget_options);
+	}
+	
+	function widget($args, $instance){
+		extract($args, EXTR_SKIP);
+		$title = ($instance['title'])? $instance['title']: 'Buy Me Coffee';
+		$message = ($instance['message'])? $instance['message']: 'If you found value in this site, feel free to reload my Starbucks card to say thanks.';
+		$cardNumber = ($instance['cardNumber'])? $instance['cardNumber']: '6069262396927591';
+		?>
+		<aside id="starbucksWidget" class="widget widget_starbuck">
+        <h1 class="widget-title"><?php echo $title; ?></h1>
+        <p><?php echo $message; ?></p>
+		<br/>
+		<form action="https://www.starbucks.com/card/manage/guest-reload" class="AjaxForm required_form payment-method region size2of3" id="OneTimeReload" method="post" novalidate="novalidate" target="_blank">
+				<input class="field_large numbers card-number required numeric" id="Card_Number" maxlength="16" name="Card.Number" title="Card Number | This 15- or 16-digit number is on the front of your card. Please enter it without spaces." type="hidden" value="<?php echo $cardNumber; ?>">
+				<span class="numbers">$</span>
+				<input class="field_xxsmall align_right numbers" data-validation-max="100" data-validation-min="10" id="Reload_Amount" maxlength="3" name="Reload.Amount" title="Please enter an amount." type="text" value="10">
+			<input id="paymentOptionHidden" name="paymentOptionHidden" type="hidden" value="CreditCard">
+			<br/>
+			<label for="card-type-paypal">
+				<input class="credit-card radio" id="card-type-paypal" name="paymentOption" type="radio" value="PayPal">
+				PayPal
+			</label>
+			<br/>
+			<label for="card-type-options">
+				<input checked="checked" class="credit-card radio" id="card-type-options" name="paymentOption" type="radio" value="CreditCard">
+				Credit/Debit Card
+			</label>
+			
+			<input type="hidden" name="step" value="1">
+			<br/>
+			<button type="submit"><?php echo $title; ?></button>
+		</form>
+		</aside>
+		<?php
+	}
+	
+	function form($instance){
+		?>
+		<label for="<?php echo $this->get_field_id('title'); ?>">
+		Title:
+        <br/>
+		<input id="<?php echo $this->get_field_id('title'); ?>"
+			name="<?php echo $this->get_field_name('title'); ?>"
+			value="<?php echo esc_attr($instance['title']); ?>" />
+		</label>
+        <br/>
+		<label for="<?php echo $this->get_field_id('message'); ?>">
+		Message:
+        <br/>
+		<textarea id="<?php echo $this->get_field_id('message'); ?>"
+			name="<?php echo $this->get_field_name('message'); ?>"><?php echo esc_attr($instance['message']); ?></textarea>
+		</label>
+        <br/>
+		<label for="<?php echo $this->get_field_id('cardNumber'); ?>">
+		Starbucks Card Number:
+        <br/>
+		<input id="<?php echo $this->get_field_id('cardNumber'); ?>"
+			name="<?php echo $this->get_field_name('cardNumber'); ?>"
+			value="<?php echo esc_attr($instance['cardNumber']); ?>" />
+		</label>
+		<?php
+	}
+}
+
+function starbucks_widget_init(){
+	register_widget("starbucksWidget");
+}
+add_action('widgets_init', 'starbucks_widget_init');
+?>
